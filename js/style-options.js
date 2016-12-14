6bdd248b390cb6f3599b600e2fd05573a199674f
Josh Walker <josh@kalamuna.com>
2016-12-13 18:22:38 -0800

    This should make the js work but it doesn't because Drupal

--- js/style-options.js
+++ js/style-options.js
@@ -6,17 +6,26 @@
 (function ($) {
   Drupal.behaviors.KalaponentsStyleOptions = {
     attach: function (context, settings) {
-      console.log(settings.kalaponents_styles);
-      var $elements = $('.field-name-field-kalaponents-type input', context);
-      console.log($elements.length);
-      $elements.change(function () {
-        var $style = $(".field-name-field-kalaponents-style select", context);
-        $style.empty(); // Remove old options.
-        $style.append($("<option></option>")
-          .attr("value", '').text('- Default -'));
-        $.each(settings.kalaponents_styles[$(this).val()], function (key,value) {
-          $style.append($("<option></option>")
-            .attr("value", key).text(value));
+      var $typeRadios = $('.field-name-field-kalaponents-type', context);
+      $typeRadios.once('kpStyleVars', function() {
+        var $radio = $(this).find('input[type=radio]');
+        $radio.on('change', function() {
+
+          var $radioParent = $radio.closest('.field-name-field-kalaponents-type'),
+              $styleSelect = $radioParent.next('.field-name-field-kalaponents-style').find('select');
+          $styleSelect.empty(); // Remove old options.
+          $styleSelect
+            .append($('<option></option>')
+              .attr('value', '-none-').text('- Default -'));
+
+          // Build a string of new options to add to the select list.
+          var options;
+          $.each(settings.kalaponents_styles[$(this).val()], function (key, value) {
+            options += '<option value="' + key + '">' + value + '</option>';
+          });
+console.log(options);
+          // Add them all at once so we're not thrashing the DOM.
+          $styleSelect.append(options);
         });
       });
     }
