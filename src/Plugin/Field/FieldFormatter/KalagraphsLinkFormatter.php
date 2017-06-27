c64f6c331d96409abbffcb0e8afe38e982941fd6
Emanuel Greucean <emanuel@greucean.me>
2017-06-27 18:33:20 +0300

    Refactor uri to url for links

--- src/Plugin/Field/FieldFormatter/KalagraphsLinkFormatter.php
+++ src/Plugin/Field/FieldFormatter/KalagraphsLinkFormatter.php
@@ -40,7 +40,7 @@ class KalagraphsLinkFormatter extends KalagraphsFieldFormatter {
     // Render links with a twig template.
     return [
       '#theme' => "kalastatic__link",
-      '#uri' => $item->getUrl(),
+      '#url' => $item->getUrl(),
       '#text' => $item->title,
       // @todo Make the class variable.
       '#class' => $classes,
