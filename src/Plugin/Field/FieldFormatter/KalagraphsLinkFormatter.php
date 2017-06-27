ee1e1aca6d400987ce3165a2a5cd78f7d012c699
Emanuel Greucean <emanuel@greucean.me>
2017-06-27 22:48:16 +0300

    Add active class to links

--- src/Plugin/Field/FieldFormatter/KalagraphsLinkFormatter.php
+++ src/Plugin/Field/FieldFormatter/KalagraphsLinkFormatter.php
@@ -3,6 +3,9 @@
 namespace Drupal\kalagraphs\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Routing\StackedRouteMatchInterface;
+use Drupal\Core\Url;
 
 /**
  * Plugin implementation of the 'kalagraphs_link' formatter.
@@ -28,23 +31,42 @@ class KalagraphsLinkFormatter extends KalagraphsFieldFormatter {
    * {@inheritdoc}
    */
   protected function viewValue(FieldItemInterface $item) {
-    $classes = [];
+    $url = $item->getUrl();
+
+    // Render links with a twig template.
+    $value = array(
+      '#theme' => "kalastatic__link",
+      '#url' => $url->toString(),
+      '#text' => $item->title,
+      '#class' => [],
+    );
+
     switch ($this->kalagraphsType) {
       case 'vertical_tabs':
-        $classes[] = 'tab';
+        $value['#class'][] = 'tab';
         break;
 
       default:
-        $classes[] = 'button';
+        $value['#class'][] = 'button';
     }
-    // Render links with a twig template.
-    return [
-      '#theme' => "kalastatic__link",
-      '#url' => $item->getUrl(),
-      '#text' => $item->title,
-      // @todo Make the class variable.
-      '#class' => $classes,
-    ];
+
+    // Figure out if link is active
+    if (!$url->isExternal()) {
+      $link_path = $url->getInternalPath();
+      $route_match = \Drupal::routeMatch();
+      if ($route_match instanceof StackedRouteMatchInterface) {
+        $route_match = $route_match->getMasterRouteMatch();
+      }
+      $current_path = $route_match->getRouteName() ? Url::fromRouteMatch($route_match)->getInternalPath() : '';
+      $is_active = ($current_path == $link_path);
+      
+      if ($is_active) {
+        $value['#class'][] = 'active';
+        $value['#active'] = TRUE;
+      }
+    }
+    
+    return $value;
   }
 
 }
