e1b9815baf9d8796ad43d60db7c46ba44aeda97a
Emanuel Greucean <emanuel@greucean.me>
2017-06-27 16:37:55 +0300

    Fix link href

--- src/Plugin/Field/FieldFormatter/KalagraphsLinkFormatter.php
+++ src/Plugin/Field/FieldFormatter/KalagraphsLinkFormatter.php
@@ -40,7 +40,7 @@ class KalagraphsLinkFormatter extends KalagraphsFieldFormatter {
     // Render links with a twig template.
     return [
       '#theme' => "kalastatic__link",
-      '#href' => $item->uri,
+      '#href' => $item->getUrl(),
       '#text' => $item->title,
       // @todo Make the class variable.
       '#class' => $classes,
