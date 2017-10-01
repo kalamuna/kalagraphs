e9f2743f452ce6dd886fc24a37b4268df18cf890
Derek DeRaps <hawkeye@tenderwolf.io>
2017-09-30 21:53:17 -0400

    Allow specifying meaningful Paragraph labels

---
+++ src/Entity/Kalagraph.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\kalagraphs\Entity;
+
+use Drupal\paragraphs\Entity\Paragraph;
+
+/**
+ * Overrides the Paragraphs module entity to facilitate various tweaks.
+ */
+class Kalagraph extends Paragraph {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function label() {
+    foreach (['title', 'text', 'subtitle', 'image', 'links'] as $name) {
+      $field = "field_kalagraphs_$name";
+      if ($this->hasField($field) && !empty($this->$field->value)) {
+        return $this->$field->value;
+      }
+    }
+    return parent::label();
+  }
+
+}
